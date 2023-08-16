2ad1354df8810b41d0025a23481ec9825b771546
toka <tokamohamed15808@gmail.com>
2023-08-16 12:24:03 -0700

    t

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_lastdigit - print last digit of number
+ *
+ * @n: takes number input
+ *
+ * Return: lastdigit
+*/
+
+int print_last_digit(int n)
+{
+	int lastdigit;
+
+	if (n < 0)
+		lastdigit = -1 * (n % 10);
+	else
+		lastdigit = n % 10;
+
+	_putchar(lastdigit + '0');
+	return (lastdigit);
+}
+
