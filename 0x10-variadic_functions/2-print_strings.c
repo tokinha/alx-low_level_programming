348122aa44dfbbe155195c851ca0c9056d7f06a7
toka <tokamohamed15808@gmail.com>
2023-09-14 07:40:43 -0700

    t

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+
+/**
+ * print_strings: with separator
+ * @separator: string separator
+ * @n: arguments number
+ * @...: string to print
+ *
+ * Return: void
+*/
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	int i = n;
+	char *str;
+	va_list ap;
+
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
+	va_start(ap, n);
+	while (i--)
+		printf("%s%s", (str = va_arg(ap, char *)) ? str : "(nil)",
+				i ? (separator ? separator : "") : "\n");
+	va_end(ap);
+}
