69554990cea3c3d99661b87cb45ec3bb0b21b792
toka <tokamohamed15808@gmail.com>
2023-09-14 07:23:05 -0700

    t

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+
+
+/**
+ * sum_them_all - sums variable arguments
+ * @n: number of argument
+ * @...: integers to sum
+ *
+ * Return:integer sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int s = 0, i = n;
+	va_list ap;
+
+	if (!n)
+		return (0);
+	va_start(ap, n);
+	while (i--)
+		s += va_arg(ap, int);
+	va_end(ap);
+	return (s);
+}
