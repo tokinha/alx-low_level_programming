a9067e69ace84a0eb252129228d717d70b3a1c30
toka <tokamohamed15808@gmail.com>
2023-09-21 12:59:20 -0700

    printf project

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * _strlen - return string length
+ * @s: string
+ *
+ * Return: integer length of a string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * print_list - print linked list
+ * @h: pointer to 1st node
+ *
+ * Return: size
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
