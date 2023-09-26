8513e994a404f4b317bc07a5f2eb45e168cd0a49
toka <tokamohamed15808@gmail.com>
2023-09-26 11:43:19 -0700

    printf project

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - free list of ints
+ * @head: head pointer
+ *
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *nd
+
+		while (head)
+		{
+			nd = head;
+			head = head->next;
+			free(nd);
+		}
+}
