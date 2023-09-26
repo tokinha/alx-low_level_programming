691f49f4ef11fb7eb131f0c344d5c933276ead23
toka <tokamohamed15808@gmail.com>
2023-09-26 12:17:27 -0700

    printf project

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * sum_listint - return sum
+ * @head: head pointer
+ *
+ * Return: sum
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->t;
+		head = head->next;
+	}
+	return (sum);
+}
