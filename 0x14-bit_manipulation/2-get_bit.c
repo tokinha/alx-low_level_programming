173c4cfd1bafd1139c196db900ef86b21650e7c3
toka <tokamohamed15808@gmail.com>
2023-09-28 15:53:38 -0700

    printf project

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include <main.h>
+
+/**
+ * get_bit - get bit
+ * @n: index number
+ * @index: getting bit
+ *
+ * Return: bit state or -1 on error
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return (-1);
+
+	return (n >> index & 1);
+}
