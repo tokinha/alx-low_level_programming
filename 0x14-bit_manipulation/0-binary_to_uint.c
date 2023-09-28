76fbaa408120875134ce4349d8166d986c3b7d70
toka <tokamohamed15808@gmail.com>
2023-09-28 15:38:43 -0700

    printf project

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts binary to int
+ * @b:binary no.
+ *
+ * Return: unsigned int form of b
+*/
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int n = 0;
+
+	if (!b)
+		return (0);
+	while (*b)
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+		n = n * 2 + (*b++ - '0');
+	}
+	return (n);
+}
