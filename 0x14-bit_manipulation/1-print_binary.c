82789f90db15379168d0cf0afb63bd318d0e1c36
toka <tokamohamed15808@gmail.com>
2023-09-28 15:48:00 -0700

    printf project

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,9 +10,9 @@ void print_binary(unsigned long int n)
 {
 	int b = sizeof(n) * 8, printed = 0;
 
-	while (bit)
+	while (b)
 	{
-		if (n & 1L << --bit)
+		if (n & 1L << --b)
 		{
 			_putchar('1');
 			printed++;
